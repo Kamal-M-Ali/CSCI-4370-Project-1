c10effe55ddd33a8c81f6676c72a3364d6e0e2dc
ruixiw1 <64241100+ruixiw1@users.noreply.github.com>
2023-09-24 19:52:57 -0400

    added union and diff

--- src/main/java/uga/cs4370/mydb/Main.java
+++ src/main/java/uga/cs4370/mydb/Main.java
@@ -22,13 +22,31 @@ public class Main
         test.insert(new Cell(2234), new Cell("John Doe"));
         test.print();
 
-        Relation test1 = new RelationImpl("Test",
+        Relation test1 = new RelationImpl("Test1",
                 new ArrayList<String>(List.of("ID", "Name", "Dept")),
                 new ArrayList<Type>(List.of(Type.INTEGER, Type.STRING, Type.STRING)));
         test1.print();
 
+        Relation test3 = new RelationImpl("Test3",
+                new ArrayList<String>(List.of("ID", "Name")),
+                new ArrayList<Type>(List.of(Type.INTEGER, Type.STRING)));
+        test3.print();
+
+        test3.insert(new Cell(1234), new Cell("Jane Doe"));
+        test3.insert(new Cell(3001), new Cell("Ada Doe"));
+        test3.insert(new Cell(3002), new Cell("Anna Doe"));
+        test3.print();
+
         RA ra = new RAImpl();
         ra.select(test, (List<Cell> row) -> row.contains(new Cell("Jane Doe"))).print();
         ra.select(test, (List<Cell> row) -> row.get(test.getAttrIndex("Name")).getAsString().equals("Jane Doe")).print();
+
+        RA unionRA = new RAImpl();
+        unionRA.union(test, test3).print();
+        unionRA.union(test, test1).print(); // not compatible
+
+        RA diffRA = new RAImpl();
+        diffRA.diff(test, test3).print();
+        diffRA.diff(test, test1).print(); // not compatible
     }
 }
--- src/main/java/uga/cs4370/mydb/impl/RAImpl.java
+++ src/main/java/uga/cs4370/mydb/impl/RAImpl.java
@@ -24,14 +24,84 @@ public class RAImpl implements RA {
         return null;
     }
 
+    /**
+     *
+     * @param rel1
+     * @param rel2
+     * @return The resulting relation after applying the union operation.
+     * @throws IllegalArgumentException //If rel1 and rel2 are not compatible.
+     */
     @Override
-    public Relation union(Relation rel1, Relation rel2) {
-        return null;
+    public Relation union(Relation rel1, Relation rel2) throws IllegalArgumentException{
+        Relation res = new RelationImpl("Union", rel1.getAttrs(), rel1.getTypes());
+        // Check compatible
+        if(rel1.getAttrs().size() == rel2.getAttrs().size()) {
+            // insert rows in Relation 1
+            for (List<Cell> row1 : rel1.getRows()) {
+                res.insert(row1);
+            }
+            // find the unrepeated rows
+            for (List<Cell> row2 : rel2.getRows()) {
+                boolean isRepeat = false;
+                for (List<Cell> row1 : rel1.getRows()) {
+                    if(row2.equals(row1)) {
+                        isRepeat = true;
+                    }
+                }
+                // insert the unrepeated rows in Relation 2
+                if(isRepeat == false) {
+                    res.insert(row2);
+                }
+            }
+        } else {
+            System.out.println("Sorry, two relation are not compatible.");
+        }
+        return res;
     }
 
+    /**
+     *
+     * @param rel1
+     * @param rel2
+     * @return The resulting relation after applying the diff operation.
+     * @throws IllegalArgumentException //If rel1 and rel2 are not compatible.
+     */
     @Override
-    public Relation diff(Relation rel1, Relation rel2) {
-        return null;
+    public Relation diff(Relation rel1, Relation rel2) throws IllegalArgumentException{
+        Relation repeat = new RelationImpl("Repeat", rel1.getAttrs(), rel1.getTypes());
+        Relation res = new RelationImpl("Diff", rel1.getAttrs(), rel1.getTypes());
+        // Check compatible
+        if(rel1.getAttrs().size() == rel2.getAttrs().size()) {
+            // insert the unique rows in Relation 1
+            for (List<Cell> row1 : rel1.getRows()) {
+                boolean isRepeat = false;
+                for (List<Cell> row2 : rel2.getRows()) {
+                    // find the repeated part
+                    if (row2.equals(row1)) {
+                        isRepeat = true;
+                        repeat.insert(row2);
+                    }
+                }
+                if (isRepeat == false) {
+                    res.insert(row1);
+                }
+            }
+            // insert the unique rows in Relation 2
+            for (List<Cell> row2 : rel2.getRows()) {
+                boolean isRepeat = false;
+                for (List<Cell> row3 : repeat.getRows()) {
+                    if (row2.equals(row3)) {
+                        isRepeat = true;
+                    }
+                }
+                if (isRepeat == false) {
+                    res.insert(row2);
+                }
+            }
+        } else {
+            System.out.println("Sorry, two relation are not compatible.");
+        }
+        return res;
     }
 
     @Override
